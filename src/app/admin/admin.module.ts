509440ab52b0808e59ceb2f55ad05320d8714f40
Fady Fransis <fady.fransis@smartbusinesstec.com>
2021-11-07 17:39:43 +0200

    delete un nicessary files

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -10,34 +10,20 @@ import { SharedModule } from '../shared/shared.module';
 import { AdminLayoutComponent } from './components/admin-layout/admin-layout.component';
 import { AdminRoutingModule } from './admin-routing.module';
 
-import { ManageClientsComponent } from './components/manage-clients/manage-clients.component';
-import { ManageOrdersComponent } from './components/manage-orders/manage-orders.component';
 import { LanguagePipe } from '../admin/pipes/lang-pipe';
-import { ManageItemsComponent } from './components/manage-items/manage-items.component';
-import { BrandsListComponent } from './components/brands/list/brands-list.component';
-import { BrandsFormComponent } from './components/brands/form/brands-form.component';
-import { VouchersListComponent } from './components/vouchers/list/vouchers-list.component';
-import { VouchersFormComponent } from './components/vouchers/form/vouchers-form.component';
-import { SpecsListComponent } from './components/Specs/list/specs-list.component';
-import { SpecsFormComponent } from './components/Specs/form/specs-form.component';
+
 import { NationsListComponent } from './components/Nations/list/nations-list.component';
 import { NationsFormComponent } from './components/Nations/form/nations-form.component';
-import { ItemDetailsComponent } from './components/item-details/item-details.component';
+
 import {CategoriesComponent} from './components/mange-Categories/list/categories.component';
 import {CategoryFormComponent} from './components/mange-Categories/form/category-form.component';
-import { ImageUploadComponent } from './components/image-upload/image-upload.component';
+
 import { AuthGuard } from './../services/config/auth.guard';
-import { ItemAccessoriesComponent } from './components/item-accessories/item-accessories.component';
+
 import { FileUploadComponent } from './components/file-upload/file-upload.component';
-import { LatestNewsFormComponent } from './components/manage-news/form/latest-news-form.component';
-import { LatestNewsListComponent } from './components/manage-news/list/latest-news-list.component';
+
 import { FilterPipe } from './pipes/filter-pipe';
-import { SliderFormComponent } from './components/mange-slider/form/slider-form.component';
-import { SliderListComponent } from './components/mange-slider/list/slider-list.component';
-import { MangeCurrenciesComponent } from './components/mange-currencies/mange-currencies.component';
-import { MyprofileComponent } from './components/myprofile/myprofile.component';
-import { ChangePasswordComponent } from './components/change-password/change-password.component';
-import { ClientDetailsComponent } from './components/client-details/client-details.component';
+
 import { StocksListComponent } from './components/stocks/list/stocks-list.component';
 import { StocksFormComponent } from './components/stocks/form/stocks-form.component';
 
@@ -48,33 +34,17 @@ import { StocksFormComponent } from './components/stocks/form/stocks-form.compon
     FooterComponent,
     DashboardComponent,
     AdminLayoutComponent,
-    ManageClientsComponent,
-    ManageOrdersComponent,
+  
     LanguagePipe,
-    ManageItemsComponent,
-    BrandsListComponent,
-    BrandsFormComponent,
-    VouchersListComponent,
-    VouchersFormComponent,
-    SpecsListComponent,
-    SpecsFormComponent,
+
     NationsListComponent,
     NationsFormComponent,
-    ItemDetailsComponent,
     CategoriesComponent,
     CategoryFormComponent,
-    ImageUploadComponent,
-    ItemAccessoriesComponent,
     FileUploadComponent,
-    LatestNewsFormComponent,
-    LatestNewsListComponent,
+
     FilterPipe,
-    SliderFormComponent,
-    SliderListComponent,
-    MangeCurrenciesComponent,
-    MyprofileComponent,
-    ChangePasswordComponent,
-    ClientDetailsComponent,
+
     StocksListComponent,
     StocksFormComponent
   ],
